e7aaefaa82b7a14f20ada6c8b3275f8c0dfbe905
Albert Stepantsev <test@pr-of-it.ru>
2016-12-28 00:21:56 +0300

    + PHP export serializer and tests

--- tests/Serializers/PhpTest.php
+++ tests/Serializers/PhpTest.php
@@ -28,8 +28,22 @@ class FileTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('3.14159', $serializer->encode(3.14159));
         $this->assertEquals('-1.2E+34', $serializer->encode(-1.2e34));
 
-        $this->assertEquals('\'foobar\'', $serializer->encode('foobar'));
-        $this->assertEquals('\'foo\\\'bar\'', $serializer->encode('foo\'bar'));
+        $this->assertEquals("'foobar'", $serializer->encode('foobar'));
+        $this->assertEquals("'foo\\'bar'", $serializer->encode('foo\'bar'));
+    }
+
+    public function testEncodeSimpleArray()
+    {
+        $serializer = new Php();
+
+        $this->assertEquals(
+            "[\n  0 => 1,\n  1 => 2,\n  2 => 3,\n]",
+            $serializer->encode([1, 2, 3])
+        );
+        $this->assertEquals(
+            "[\n  'foo' => 100,\n  'bar' => 200,\n  'baz' => 300,\n]",
+            $serializer->encode(['foo' => 100, 'bar' => 200, 'baz' => 300])
+        );
     }
 
 }
